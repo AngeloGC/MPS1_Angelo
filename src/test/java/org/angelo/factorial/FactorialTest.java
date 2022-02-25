da3c660946eccc1f70d88075ba612c278f135162
Angelo <angelogorgone2704@outlook.es>
2022-02-25 11:26:46 +0100

    Commit

---
+++ src/test/java/org/angelo/factorial/FactorialTest.java
@@ -0,0 +1,68 @@
+package org.angelo.factorial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test cases:
+ * factorial 0 -> 1 *
+ * factorial 1 -> 1 *
+ * factorial 2 -> 2 *
+ * factorial 3 -> 6 *
+ * factorial 6 -> 720 *
+ * factorial negative numer ->
+ */
+
+class FactorialTest {
+    @Test
+    public void shouldComputeReturnOneIfTheNumberIsZero() {
+        var factorial = new Factorial();
+        int expectedValue = 1;
+        int obtainedValue = factorial.compute(0);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeReturnOneIfTheNumberIsOne() {
+        var factorial = new Factorial();
+        int expectedValue = 1;
+        int obtainedValue = factorial.compute(1);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeReturnTwoIfTheNumberIsTwo() {
+        var factorial = new Factorial();
+        int expectedValue = 2;
+        int obtainedValue = factorial.compute(2);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeReturnSixIfTheNumberIsThree() {
+        var factorial = new Factorial();
+        int expectedValue = 6;
+        int obtainedValue = factorial.compute(3);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeReturn720IfTheNumberIsSix() {
+        var factorial = new Factorial();
+        int expectedValue = 720;
+        int obtainedValue = factorial.compute(6);
+
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldComputeOfANegativeNumberRaiseAnException() {
+        var factorial = new Factorial();
+        assertThrows(RuntimeException.class, () -> factorial.compute(-1));
+    }
+}
